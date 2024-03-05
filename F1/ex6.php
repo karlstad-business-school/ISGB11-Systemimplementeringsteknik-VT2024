4faf8b17ea84e628b04f883c69eae7a9a2cdca52
peteratkau <59958667+peteratkau@users.noreply.github.com>
2024-03-05 08:54:22 +0100

    New files

---
+++ F1/ex6.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en" >
+	<head>
+		<meta charset="utf-8" />
+		<title>F1 Exempel 6</title>
+	</head>
+	<body>
+		<div>
+		
+			
+			<?php
+				echo( "<pre>" );
+				print_r( $_POST );
+				echo( "</pre>" );
+			?>	
+			<?php echo( $_SERVER["PHP_SELF"] ); ?>
+
+			<form action="ex6.php" method="post"> 
+				<input type="submit" name="btnSend" value="Send" />
+				<input type="submit" name="btnDemo" value="Demonstration" />
+				<input type="hidden" name="hidDemo" value="Alltid med..." />
+			</form>
+
+			<?php
+				//Här kommer koden...
+
+				/*
+					Att öva på egen hand.
+
+					Kontrollera om en specifik submit-knapp är tryck och skriv ut värdet som kommer till servern.
+					
+				*/
+			?>
+			
+		</div>
+	</body>
+</html>
---
+++ F1/ex7.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en" >
+	<head>
+		<meta charset="utf-8" />
+		<title>F1 Exempel 7</title>
+		<style>
+			img { width: 10%; height: 10%; }
+		</style>
+		
+	</head>
+	<body>
+		<div>
+
+			<?php
+				echo ("<pre>" );
+				print_r( $_GET );
+				echo( "</pre>" );
+			?>
+			
+			<?php echo( $_SERVER["PHP_SELF"] ); ?>
+			<a href="ex7.php?linkSend=true&linkTest=Alltid med...">Send</a>
+			<a href="ex7.php?linkDemo=true&linkTest=alltid med...">Demonstraion</a>
+		
+			<?php
+				//Här kommer koden...
+
+				/*
+					Att öva på egen hand.
+
+					Kontrollera om en specifik länk är tryck och skriv ut värdet som kommer till servern.
+					
+				*/
+			?>
+			
+		</div>
+	</body>
+</html>
